66201cee2eec2bb035e216cfb10016f8c5bfec5c
Danny Kim <dudkny@gmail.com>
2019-11-09 12:03:40 -0600

    updating connection

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,7 +19,7 @@ class Home extends Component {
     alert: ''
   };
   componentDidMount() {
-    const socket = openSocket();
+    const socket = openSocket('https://bookgle.herokuapp.com/');
     socket.on('save', (data) => {
       console.log(data)
 
